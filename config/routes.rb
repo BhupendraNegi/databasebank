6669e519f3c8b079952ec872326242a69435494b
Bhupendra Singh Negi <bhupendranegi92@gmail.com>
2023-08-11 14:26:35 +0530

    home page setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'main#index'
   devise_for :users
+  resources :home, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  match '/', to: 'home#index', via: :all
+  match '/home/*path', to: 'home#index', via: :all
+  match '/', to: 'main#index', via: :all
 end
